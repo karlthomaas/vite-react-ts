cfc70699f0e1ffdf84f3bf14e6b7eb0c15fb4eaf
Karl-Thomas <karl-thomas@zink.ee>
2024-03-24 22:33:32 +0200

    Setup ShadCN and apply theme

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
@@ -5,6 +6,11 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   plugins: [react()],
 })
